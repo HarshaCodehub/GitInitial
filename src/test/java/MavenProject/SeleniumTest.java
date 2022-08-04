3ee32acc636c887358bc9417da601d042a78d928
Harsha <msriharsha903@gmail.com>
2022-08-04 09:56:09 +0530

    This is commit from Indian man

--- src/test/java/MavenProject/SeleniumTest.java
+++ src/test/java/MavenProject/SeleniumTest.java
@@ -7,10 +7,14 @@ public class SeleniumTest {
 	public void browserAutomation()
 	{
 		System.out.print("browserAutomation");
+		System.out.print("browserAutomation2");
+		System.out.print("browserAutomation3");
 	}
 	@Test
 	public void elementsUi()
 	{
 		System.out.print("elementsUi");
+		System.out.print("elementsUi2");
+		System.out.print("elementsUi3");
 	}
 }
